e37efe0dc4e5f5b832e6780648cdf891b367d45f
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-02-18 18:16:28 +0000

    dice game done

--- programs/dice/src/contexts/resolve_bet.rs
+++ programs/dice/src/contexts/resolve_bet.rs
@@ -1,3 +1,5 @@
+use std::fmt::UpperExp;
+
 use anchor_instruction_sysvar::Ed25519InstructionSignatures;
 use anchor_lang:: {
     prelude::*,
@@ -7,7 +9,7 @@ use solana_program::{
     ed25519_program, hash::hash, sysvar::instructions::load_instruction_at_checked,
 };
 
-use crate::state::Bet;
+use crate::{error::CustomError, state::Bet};
 
 #[derive(Accounts)]
 pub struct ResolveBet<'info> {
@@ -31,10 +33,79 @@ pub struct ResolveBet<'info> {
     #[account(
         address = solana_program::sysvar::instructions::ID
     )]
-    pub instruction_sysvar: AccountInfo<'info>,
+    /// CHECK:
+    pub instructions_sysvar: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> ResolveBet<'info> {
-    
+    pub fn verify_ed25519_signature(&mut self, sig: &[u8]) -> Result<()> {
+        let ix = 
+        load_instruction_at_checked(0, &self.instructions_sysvar.to_account_info())?;
+
+        require_keys_eq!(ix.program_id, ed25519_program::ID, CustomError::CustomError);
+
+        require_eq!(ix.accounts.len(), 0, CustomError::CustomError); 
+        let signatures = Ed25519InstructionSignatures::unpack(&ix.data)?.0;
+
+        require_eq!(signatures.len(), 1, CustomError::CustomError);
+        let signature_in_program = &signatures[0];
+
+        require!(signature_in_program.is_verifiable, CustomError::CustomError);
+
+        require_keys_eq!(
+            signature_in_program.public_key.unwrap(),
+            self.house.key(),
+            CustomError::CustomError
+        );
+
+        require!(
+            signature_in_program.signature.unwrap().eq(sig),
+            CustomError::CustomError
+        );
+
+        require!(
+            signature_in_program.message.as_ref().unwrap().eq(&self.bet.to_slice()),
+            CustomError::CustomError
+        );
+
+        Ok(())
+    }
+
+    pub fn resolve_bet(&mut self, sig: &[u8], bumps: &ResolveBetBumps) -> Result<()> {
+        let hash = hash(sig).to_bytes();
+
+        let mut hash_16: [u8; 16] = [0; 16];
+        hash_16.copy_from_slice(&hash[0..16]);
+        let lower = u128::from_le_bytes(hash_16);
+
+        hash_16.copy_from_slice(&hash[16..32]);
+        let upper = u128::from_le_bytes(hash_16);
+
+        let roll: u8 = lower.wrapping_add(upper).wrapping_rem(100) as u8 + 1;
+
+        if self.bet.roll > roll {
+            let payout = (self.bet.amount as u128)
+                .checked_mul(1000 - 150 as u128)
+                .unwrap()
+                .checked_div(self.bet.roll as u128)
+                .unwrap()
+                .checked_div(10000)
+                .unwrap() as u64;
+            let cpi_program = self.system_program.to_account_info();
+
+            let cpi_accounts = Transfer {
+                to: self.vault.to_account_info(),
+                from: self.player.to_account_info(),
+            };
+
+            let seeds = [b"vault", &self.house.key().to_bytes()[..], &[bumps.vault]];
+            let signer_seeds = &[&seeds[..]][..];
+
+            let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+            transfer(cpi_context, payout)?;
+        }
+        Ok(())
+    }
 }
